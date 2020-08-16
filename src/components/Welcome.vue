e455c8afb3c7e9a16b244af8260f06321012d4d5
Othon Ferreira <othondev@gmail.com>
2020-08-16 05:17:12 +0000

    Added repository link

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,12 +4,14 @@
       Welcome to my Developer Coding Test.
     </p>
     <p class="text-center font-weight-medium ">
-      After check if please visit my
-      linkedin or call me I am completly available to answer any question that
-      you could have.
+      After check if please visit my linkedin or call me I am completly
+      available to answer any question that you could have.
     </p>
     <p class="text-center font-weight-medium ">
       <a :href="myLinkedin"> My Linkedin </a>
+    </p>
+    <p class="text-center font-weight-medium ">
+      <a :href="myRepository"> Repository here </a>
     </p>
   </v-container>
 </template>
@@ -19,7 +21,8 @@ export default {
   name: "Welcome",
 
   data: () => ({
-      myLinkedin: "https://www.linkedin.com/in/othon-ferreira-b0b6a888/"
-    }),
+    myLinkedin: "https://www.linkedin.com/in/othon-ferreira-b0b6a888/",
+    myRepository: "https://github.com/othondev/techppl",
+  }),
 };
 </script>
